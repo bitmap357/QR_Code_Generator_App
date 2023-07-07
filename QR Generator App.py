3d10a9e7f651e2fc330985fe193e4b5473e090ce
bitmap357 <edakoyeram@gmail.com>
2023-07-07 13:54:04 +0000

    function to generate qr code

--- QR Generator App.py
+++ QR Generator App.py
@@ -4,4 +4,18 @@ from PIL import ImageTk, Image
 
 root = Tk()
 
+
+def generate():
+    link_name = name_entry.get()
+    link = link_entry.get()
+    file_name = link_name + ".png"
+    url = pyqrcode.create(link)
+    url.png(file_name, scale=8)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 450, window=image_label)
+
+
+
 root.mainloop()
